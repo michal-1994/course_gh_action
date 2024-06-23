da817c9bdb5c87b523a2c3f409f394d1aacdfdf9
Michal <michalgrzegorczyk04@gmail.com>
2024-06-23 11:31:19 +0200

    revert changes

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-// This is important!
-// Other change
-// Add another comment
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
