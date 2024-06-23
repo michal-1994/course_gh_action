a6c2073cb023ad9a573c6b164ccddb9bf31e0275
Michal <michalgrzegorczyk04@gmail.com>
2024-06-23 11:00:17 +0200

    change header in help box

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -3,7 +3,7 @@ import './HelpBox.css';
 function HelpBox({ title, text }) {
   return (
     <article className="help-box">
-      <h2>{title}</h2>
+      <h3>{title}</h3>
       <p>{text}</p>
     </article>
   );
