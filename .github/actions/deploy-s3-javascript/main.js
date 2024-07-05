b49ac093a6eb4e56118cee40a3cf92f31199591c
Michal <michalgrzegorczyk04@gmail.com>
2024-07-05 11:12:48 +0200

    fixes

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,26 +1,20 @@
-const core = require("@actions/core");
-// const github = require("@actions/github");
-const exec = require("@actions/exec");
+const core = require('@actions/core');
+// const github = require('@actions/github');
+// const exec = require('@actions/exec');
 
 function run() {
-  // const bucket = core.getInput("bucket", {
-  //   required: true,
-  // });
-  // const bucketRegion = core.getInput("bucket-region", {
-  //   required: true,
-  // });
-  // const distFolder = core.getInput("dist-folder", {
-  //   required: true,
-  // });
+  // const bucket = core.getInput('bucket', { required: true });
+  // const bucketRegion = core.getInput('bucket-region', { required: true });
+  // const distFolder = core.getInput('dist-folder', { required: true });
 
   // const s3Uri = `s3://${bucket}`;
   // exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
-  const websiteUrl = 'websiteUrl';
-  // const websiteUrl = `https://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
-  core.setOutput('Website url', websiteUrl)
-
+  // const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+  const websiteUrl = `websiteUrl`;
+  core.setOutput('website-url', websiteUrl); // ::set-output
   core.notice("Hello from my custom JS Action!");
 }
 
 run();
+
