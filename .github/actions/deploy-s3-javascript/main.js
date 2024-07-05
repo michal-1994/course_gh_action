aa52e00abe8cada69d2c90529db4de92c36fe232
Michal <michalgrzegorczyk04@gmail.com>
2024-07-05 11:05:50 +0200

    add output and comment not used code

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,18 +3,22 @@ const core = require("@actions/core");
 const exec = require("@actions/exec");
 
 function run() {
-  const bucket = core.getInput("bucket", {
-    required: true,
-  });
-  const bucketRegion = core.getInput("bucket-region", {
-    required: true,
-  });
-  const distFolder = core.getInput("dist-folder", {
-    required: true,
-  });
+  // const bucket = core.getInput("bucket", {
+  //   required: true,
+  // });
+  // const bucketRegion = core.getInput("bucket-region", {
+  //   required: true,
+  // });
+  // const distFolder = core.getInput("dist-folder", {
+  //   required: true,
+  // });
 
-  const s3Uri = `s3://${bucket}`;
-  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+  // const s3Uri = `s3://${bucket}`;
+  // exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
+  const websiteUrl = 'websiteUrl';
+  // const websiteUrl = `https://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+  core.setOutput('Website url', websiteUrl)
 
   core.notice("Hello from my custom JS Action!");
 }
